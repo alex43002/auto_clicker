8cf56352386b773c8a6fb782c29b9b0bc0064c39
Alexander DeJesus <alexdejesus336@Yahoo.com>
2024-04-11 17:41:39 -0400

    Refactored codebase

--- Program.cs
+++ Program.cs
@@ -8,33 +8,48 @@ namespace mouse_clicker_app
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Mouse Clicker App!");
-            Console.WriteLine("Enter X coordinate:");
-            int.TryParse(Console.ReadLine(), out int xCoord);
 
-            Console.WriteLine("Enter Y coordinate:");
-            int.TryParse(Console.ReadLine(), out int yCoord);
+            // Get input from the user
+            int xCoord = GetUserInput("Enter X coordinate:");
+            int yCoord = GetUserInput("Enter Y coordinate:");
+            int clickCount = GetUserInput("Enter number of clicks:");
 
-            Console.WriteLine("Enter number of clicks:");
-            int.TryParse(Console.ReadLine(), out int clickCount);
-
+            // Simulate mouse clicks
             SimulateMouseClick(xCoord, yCoord, clickCount);
 
             Console.WriteLine("Mouse clicks simulated successfully!");
         }
 
+        // Method to get user input
+        static int GetUserInput(string prompt)
+        {
+            int input;
+            while (true)
+            {
+                Console.WriteLine(prompt);
+                if (int.TryParse(Console.ReadLine(), out input))
+                {
+                    return input;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                }
+            }
+        }
+
+        // Method to simulate mouse clicks
         static void SimulateMouseClick(int x, int y, int clickCount)
         {
             var inputSimulator = new InputSimulator();
-            inputSimulator.Mouse
-                .MoveMouseToPositionOnVirtualDesktop(x, y)
-                .LeftButtonClick();
-
-            // If you want to perform multiple clicks
-            for (int i = 1; i < clickCount; i++)
+            for (int i = 0; i < clickCount; i++)
             {
                 inputSimulator.Mouse
                     .MoveMouseToPositionOnVirtualDesktop(x, y)
                     .LeftButtonClick();
+
+                // Add a small delay between clicks
+                System.Threading.Thread.Sleep(100);
             }
         }
     }
