66a9ac7a6dfa485b4b2d56e53d8f62d1459c77e4
Alexander DeJesus <alexdejesus336@Yahoo.com>
2024-04-11 17:11:19 -0400

    Basical cmd version working

--- Program.cs
+++ Program.cs
@@ -1,9 +1,41 @@
 ﻿using System;
+using WindowsInput;
 
-class Program
+namespace mouse_clicker_app
 {
-    static void Main()
+    class Program
     {
-        Console.WriteLine("Hello, world!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mouse Clicker App!");
+            Console.WriteLine("Enter X coordinate:");
+            int xCoord = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Y coordinate:");
+            int yCoord = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number of clicks:");
+            int clickCount = int.Parse(Console.ReadLine());
+
+            SimulateMouseClick(xCoord, yCoord, clickCount);
+
+            Console.WriteLine("Mouse clicks simulated successfully!");
+        }
+
+        static void SimulateMouseClick(int x, int y, int clickCount)
+        {
+            var inputSimulator = new InputSimulator();
+            inputSimulator.Mouse
+                .MoveMouseToPositionOnVirtualDesktop(x, y)
+                .LeftButtonClick();
+
+            // If you want to perform multiple clicks
+            for (int i = 1; i < clickCount; i++)
+            {
+                inputSimulator.Mouse
+                    .MoveMouseToPositionOnVirtualDesktop(x, y)
+                    .LeftButtonClick();
+            }
+        }
     }
 }
